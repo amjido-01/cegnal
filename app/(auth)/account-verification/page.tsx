22477498e4dc745a166a29de84817706071ede20
Abubakar Muhammad Ala <youndsadeeq10@gmail.com>
2025-08-21 10:38:41 +0100

    account-verification

---
+++ app/(auth)/account-verification/page.tsx
@@ -0,0 +1,11 @@
+import { AccountVerificationForm } from "@/components/account-verification"
+
+export default function AccountVerificationPage() {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center px-6">
+      <div className="w-full max-w-md">
+          <AccountVerificationForm />
+        </div>
+      </div>
+  )
+}
